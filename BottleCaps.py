04edc8d82ad3e6a619a8a6ff672f1e86f69ee02f
CodeIsJustLikeMagic <CodeIsJustLikeMagic@web.de>
2020-12-11 18:29:42 +0100

    Initial commit

---
+++ BottleCaps.py
@@ -0,0 +1,11 @@
+import datetime
+import shutil
+
+numpy, ceras, tensorflow,tensorflow, patplotlip, opencv, numpy
+import cv2
+import numpy as np
+import matplotlib
+
+if __name__ == "__main__":
+
+vid = cv2.VideoCapture('')
