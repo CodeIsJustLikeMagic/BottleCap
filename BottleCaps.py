598a2f011bede2f2796ea006bc8d02ac7a104fec
CodeIsJustLikeMagic <CodeIsJustLikeMagic@web.de>
2020-12-17 17:22:54 +0100

    choose best sequence

--- BottleCaps.py
+++ BottleCaps.py
@@ -4,8 +4,8 @@ import shutil
 #numpy, ceras, tensorflow,tensorflow, patplotlip, opencv, numpy
 import cv2
 import numpy as np
-import matplotlib
 from matplotlib import pyplot as plt
+import statistics
 def h_show(title, image):
     resize = True
     height = image.shape[0]
@@ -13,13 +13,12 @@ def h_show(title, image):
     # image = cv2.resize(image, (width, height))
     if resize:
         image = cv2.resize(image, (int(width * 0.5), int(height * 0.5)))
-    #cv2.imshow(title, image)
+    cv2.imshow(title, image)
 
 #return a quantified differnece between two frames
 
-def plotdiffs(vid):
-    fps = vid.get(cv2.CAP_PROP_FPS)
-    frame_count = vid.get(cv2.CAP_PROP_FRAME_COUNT)
+def diffsbetweenallframes(vid):
+
     diffs = np.array([])
 
     vid.set(cv2.CAP_PROP_POS_FRAMES, 0)
@@ -32,7 +31,8 @@ def plotdiffs(vid):
         diffs = np.append(diffs, dif)
         prevframe = currentframe
     h_plot(diffs, "mean square diffs", 0)
-    findszene(diffs)
+    return diffs
+
 def diffofdiffs(diffarr):
     #similarity to previous error value
     diffofdiffs = np.array([])
@@ -44,9 +44,9 @@ def diffofdiffs(diffarr):
 
     h_plot(diffofdiffs, "diffofdiffs")
 
-def findszene(diffarr):
-    minaountofframes = 5
-    maxdiff = 20
+def findsequences(diffarr):
+    minaountofframes = 10
+    global maxdiff
 
     # I want seqzenzes of this were the diff is small, how many frames? how small is it?
     lines= []
@@ -67,13 +67,56 @@ def findszene(diffarr):
     if(len(currentline) > minaountofframes):
         lines.append(currentline)
         linestarts.append(currentlinestart)
-    print(len(lines), len(linestarts))
+    #print(len(lines), len(linestarts))
     for line,linest in zip(lines, linestarts):
         h_plot(line, "line "+str(linest), linest)
         #current diff,  currentline, start index, mean diff in line, length
-    #there should be one szene right at the start, one in the middle and one at the end
-    print("found", len(lines), "sequenzes")
+    #print("found", len(lines), "sequenzes")
+    return linestarts, lines
 
+    
+def getframeindeces(linestarts, lines):
+    #there should be one szene right at the start, one in the middle and one at the end
+    if 0 in linestarts:
+        linestarts.pop(0)
+        lines.pop(0)
+        # the sequzence at the start is worse than the main sequenze.
+        # now there should be one or two good seqzences.
+        return 0, findbestlines(linestarts, lines)
+    else:
+        print("AHHHHH there is no seqzence at the start, pls help")
+        return 0, findbestlines(linestarts, lines)
+
+
+    # remove bas sequenzs until we only have two left.
+    # one should be in the middle or at the end if there are only two good sequences
+
+def findbestlines(linestarts, lines):
+    nlines = []
+    for line, linest in zip(lines, linestarts):
+        #print(linest, np.std(line), np.mean(line))
+        frameindeces = np.array(range(linest, linest+len(line)))
+        line = np.vstack([line, frameindeces])
+        nlines.append(line)
+    #print(nlines)
+    nlines.sort(key=sortbymean)
+    #so now the first or the second one is our sequence
+    besttwo = nlines[:2]
+    #if the second line mean is muuch worse than the first one there is probably one one static szene
+    besttwo.sort(key=smallerstart)
+    #print(besttwo)
+    #choose the frame with the smallest score from the first one
+    bestone = besttwo[0]
+    #print('startframe of bestone', bestone[1][0])
+    bestone = zip(bestone[0], bestone[1])
+    m = min(bestone, key=lambda t: t[0])
+    return m[1]
+
+def smallerstart(elem):
+    return elem[1][0]
+
+def sortbymean(elem):
+    return np.mean(elem[0])
 
 def h_plot(arr, desciption):
     h_plot(arr, desciption, 0)
@@ -81,22 +124,20 @@ def h_plot(arr, desciption):
 def h_plot(arr, description, start):
     fig, ax = plt.subplots(figsize=(5, 4))
     x = np.arange(start, len(arr) + start)
-    ax.scatter(x, arr, label='diffs between frames')
+    ax.scatter(x, arr, marker=',', label='diffs between frames', s=4)
     ax.set_ylabel(description)
+    global maxdiff
+    ax.axhline(y=maxdiff, color = "black");
     plt.show()
 
 def mse(imageA, imageB):
     # the 'Mean Squared Error' between the two images is the
     # sum of the squared difference between the two images;
-    # NOTE: the two images must have the same dimension
     err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
     err /= float(imageA.shape[0] * imageA.shape[1])
+    return err  # return the MSE, the lower the error, the more "similar" the two images are
 
-    # return the MSE, the lower the error, the more "similar"
-    # the two images are
-    return err
-
-def diffinframes(frame1, frame2):
+def diffinframes(frame1, frame2):# is really slow
     frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
     frame2= cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
 
@@ -107,8 +148,6 @@ def diffinframes(frame1, frame2):
     #I wanted to use templatemaching but atm I use simple dif
     return quanti
 
-
-
 def readVideo(vid):
     while vid.isOpened():
         ret, frame = vid.read()
@@ -116,29 +155,68 @@ def readVideo(vid):
             break
 
 from datetime import datetime
+import os
+
+def testAllVideos():
+    #estimate every image in the folder
+    folder = 'Videos'
+    files = os.listdir(folder)
+    paths = [folder+'/' + file for file in files]
+    for path, file in zip(paths, files):
+        if file.endswith('.mp4'):
+            testVideo(path, file)
+
+def readframe(vid, frame_number):
+    vid.set(cv2.CAP_PROP_POS_FRAMES, int(frame_number))  # optional
+    success, frame = vid.read()
+    if success:
+        return frame
+    return False
+
+def difbetweenFrames(emptyframe, fullframe):
+    difference = cv2.subtract(emptyframe, fullframe)
+    h_show("difference",difference)
+    pass
+
+def testVideo(path,file):
+    print('start', path, '...')
+    now = datetime.now()
+    vid = cv2.VideoCapture(path)  # 50 is black
+    if not vid.isOpened():
+        print("Error opening video stream or file")
+
+    fps = vid.get(cv2.CAP_PROP_FPS)
+    #frame_count = vid.get(cv2.CAP_PROP_FRAME_COUNT)
+    #print("videodata: fps: ", fps, "framecount: ", frame_count)
+    diffs = diffsbetweenallframes(vid)
+    linestarts, lines = findsequences(diffs)
+    if len(lines) < 1:
+        print('to much action, threshhold excluded  everything')
+        return
+    emptyscene, fullscene = getframeindeces(linestarts, lines)
+    emptyframe = readframe(vid, fullscene)
+    fullframe = readframe(vid, fullscene)
+    print('choose frame: ', fullscene, ' min ', fullscene/fps)
+    cv2.imwrite('Results/'+file[:-3]+'png', fullframe)
+    vid.release()
+    difbetweenFrames(emptyframe, fullframe)
+    print("end. time for", file, datetime.now()-now)
+
+
+maxdiff = 50
 
 
 if __name__ == "__main__":
 
     now = datetime.now()
-    vid = cv2.VideoCapture('Videos/CV20_video_50.mp4') #50 is black
-    if not vid.isOpened():
-        print("Error opening video stream or file")
 
-    fps = vid.get(cv2.CAP_PROP_FPS)
-    frame_count = vid.get(cv2.CAP_PROP_FRAME_COUNT)
-    print("videodata: fps: ", fps, "framecount: ", frame_count)
 
     #readVideo(vid)
     #showVideo(vid)
-    plotdiffs(vid)
-    #vid.set(cv2.CV_CAP_PROP_POS_FRAMES, frame_number = 1)
-    #ret, frame1 = vid.read()
-    #vid.set(cv2.CV_CAP_PROP_POS_FRAMES, frame_number = 2)
-    #ret, frame2 = vid.read()
-    #diffinframes(frame1, frame2)
+    #testAllVideos()
+    testVideo("Videos/CV20_video_3.mp4", "CV20_video_3.mp4")
+
     print("total time", datetime.now()-now)
-    cv2.waitKey(0)
-    vid.release()
+
     cv2.destroyAllWindows()
 
